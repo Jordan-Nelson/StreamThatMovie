087b0288b2ee48150acbdd5b28bd78e26f608220
jordan nelson <jordannelson@jordans-MacBook-Pro.local>
2017-08-15 22:38:42 -0400

    Add methods to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "app/shared/user";
+import { UserService } from "app/shared/user.service";
+import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'stream-login',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  Router: Router;
+  signupError: any;
+  loginError: any;
+  UserService: UserService;
+  
+  constructor(UserService: UserService, Router: Router) {
 
-  constructor() { }
+    this.UserService = UserService;
+    this.Router = Router;
+
+  }
+
+  onLogin(loginForm: NgForm) {
+    this.UserService.login(loginForm.value)
+    .subscribe((resp: any) => {
+      console.log(resp)
+      this.Router.navigateByUrl('/')
+    }, err => {
+      this.loginError = err.json().errors;
+    })
+  }
+
+  onSignup(signupForm: NgForm) {
+    this.UserService.signup(signupForm.value)
+      .subscribe((resp: any) => {
+        this.Router.navigateByUrl('/')
+      }, err => {
+        this.signupError = err.json().errors;
+      })
+  }
+
 
   ngOnInit() {
+
+    // this.user = this.UserService.user.subscribe()
+
   }
 
 }
